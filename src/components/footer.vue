cf0f8fa50320069b3f9b796e222116c62a8edeae
MiguelDeVeyra <[work.deveyrajm@gmail.com]>
2020-05-15 16:00:40 +0800

    Footerv2

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,86 +1,38 @@
 <template lang="html">
   <section class="mt-12">
-    <v-footer
-        padless
-        color="green lighten-1 white--text"
-      >
-        <v-row class="mt-5 mx-8" wrap>
-          <v-col cols="12" md="2">
-            <h5 class="title mb-3 ml-3">Information</h5>
-            <div v-for="link in infolinks" :key="link.name">
-              <v-btn
-                small
-                depressed
-                color="green lighten-1"
-                class="info__btn my-1"
-                >
-                {{link.name}}
-              </v-btn><br>
-            </div>
-          </v-col>
-          <v-col cols="12" md="2">
-            <h5 class="title mb-2 ml-3">Support</h5>
-            <div v-for="link in supplinks" :key="link.name">
-              <v-btn
-                small
-                depressed
-                color="green lighten-1"
-                class="info__btn my-1"
-                >
-                {{link.name}}
-              </v-btn><br>
-            </div>
-          </v-col>
-          <v-col cols="12" md="2">
-            <h5 class="title mb-2 ml-3">Contact Us</h5>
-            <div class="">
-              <p class="ml-3 body-2">
-                Level 10-1, Global Place,
-                25th Street cor. 5th Avenue, 1630
-                Bonifacio Global City
-                Taguig City, Metro Manila
-              </p>
-              <v-btn
-                small
-                depressed
-                color="green lighten-1"
-                class="info__btn my-1"
-                >
-                Message Us
-              </v-btn><br>
-            </div>
-          </v-col>
-          <v-col cols="12" md="4" class="ml-4">
-            <h5 class="title">Partners</h5>
-            <div class="my-2">
-              <a href="#" class="mr-6 my-1"><img src="/dti.png" alt="" height="40px"></a>
-              <a href="#" class="mr-6 my-1"><img src="/nego.png" alt="" height="40px"></a>
-              <a href="#" class="mr-6 my-12"><img src="/air.png" alt="" height="40px"></a>
-            </div>
-            <h5 class="title">Payment</h5>
-            <div class="my-2">
-              <a href="#" class="mr-6"><img src="/drag.png" alt="" height="40px"></a>
-              <a href="#" class="mr-6"><img src="/pay.png" alt="" height="40px"></a>
-              <a href="#" class="mr-6"><img src="/visa.png" alt="" height="40px"></a>
-              <a href="#" class="mr-6"><img src="/mast.png" alt="" height="40px"></a>
-            </div>
-          </v-col>
-          <v-col cols="12" md="1">
-            <h5 class="">Social</h5>
-            <div class="my-2">
-              <a href="#" class="fa fa-facebook"></a>  <a href="#" class="fa fa-linkedin"></a>
-            </div>
-          </v-col>
-
-        </v-row>
-        <v-row>
-          <v-col
-            class="green darken-1 py-4 text-center white--text"
-            cols="12"
-          >
-          © 2018-2020 Zeend. All Rights Reserved.
-          </v-col>
-        </v-row>
+    <v-footer color="grey lighten-4 black--text">
+      <v-row class="mx-auto mt-2">
+        <v-col cols="12" md="3" class="offset-1">
+          <h2 class="footer__header__title">Support</h2>
+          <p><a href="#">Help</a></p>
+          <p><a href="#">FAQs</a></p>
+          <p><a href="#">Zeend Wallet</a></p>
+          <p><a href="#">Payment Methods</a></p>
+          <p><a href="#">Contact Us</a></p>
+        </v-col>
+        <v-col cols="12" md="3" class="ZeendAbt">
+          <h2 class="footer__header__title">Zeend</h2>
+          <p><a href="#">About Us</a></p>
+          <p><a href="#">Zeend Blog</a></p>
+          <p><a href="#">Privacy Policy</a></p>
+          <p><a href="#">Merchant Account</a></p>
+          <p><a href="#">Terms and Conditions</a></p>
+        </v-col>
+        <v-col cols="12" md="5" class="AppDL">
+          <div class="app__head">
+            <h3 class="footer__header__title">Download the App</h3>
+          </div>
+        </v-col>
+        <v-divider></v-divider>
+        <v-col cols="12" md="5" class="offset-1">
+          <span class="body-1">Follow us on: </span><i class="fa fa-facebook mr-2" aria-hidden="true"></i><i class="fa fa-linkedin" aria-hidden="true"></i>
+        </v-col>
+        <v-col cols="12" md="5" class="mt-2">
+          <div class="text-center">
+            © 2018-2020 Zeend. All Rights Reserved.
+          </div>
+        </v-col>
+      </v-row>
     </v-footer>
   </section>
 </template>
@@ -91,18 +43,7 @@ export default {
 
   data(){
     return {
-      drawer:false,
 
-        infolinks: [
-          {name: 'Terms and Conditions', route: '/tnc'},
-          {name: 'Privacy Policy', route: '/pripo'},
-          {name: 'About us', route: '/about'},
-          {name: 'Blogs', route: '/blog'},
-        ],
-        supplinks:[
-          {name: 'FAQs', route: '/faqs'},
-          {name: 'Help', route: '/Help'},
-        ],
     }
   },
 
@@ -115,32 +56,56 @@ export default {
 
 <style scoped>
 
-.infor__section{
-  margin-left: 2vw;
+a{
+  text-decoration: none;
+  color: black!important;
+  font-size: .8em;
+}
+a:hover{
+  color: #EC614B!important;
 }
 
-.info__btn{
-  color: #FFF;
+p{
+  margin: 0 !important;
 }
-.fa {
-  font-size: 10px;
+.footer__header__title{
+  font-size: 1.1em;
+  font-weight: lighter;
+  margin-bottom:  1vh;
+}
+.app__head{
+  margin-top: 8vh;
+}
+@media (max-width:900px){
+    .app__head{
+      margin-top: 0;
+    }
+}
+
+@media (max-width:900px){
+  .ZeendAbt, .AppDL{
+    margin-left: 8vw;
+  }
+}
+
+.fa{
+  border-radius: 50%;
+  font-size: .8em;
   width: 3em;
   height: 3em;
-  padding-top: 1em;
   text-align: center;
-  text-decoration: none;
-  border-radius: 50%;
-}
-.fa:hover {
-    opacity: 0.7;
+  padding-top: 13px
 }
 
-.fa-facebook {
+.fa-facebook{
   background: #3B5998;
   color: white;
+  vertical-align: middle;
 }
-.fa-linkedin {
+
+.fa-linkedin{
   background: #007bb5;
   color: white;
+  vertical-align: middle;
 }
 </style>
